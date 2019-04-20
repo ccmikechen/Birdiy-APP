5100e704c8be357bb82798dee92d52e77b2681fd
Mike Chen <ccmikechen@gmail.com>
2019-04-20 22:02:36 +0800

    Delete useless mocks

--- src/screens/ProfileScreen/mocks.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-
-export const profile = {
-  name: '蘭飛鴻',
-  image: 'http://mmbiz.qpic.cn/mmbiz/10H34ZvubVOGfVkKI3YWzUMicPEUib5mEYCbFoB35808n5YibltxTEsyCqazWbvlUnzTiadOMrG2gesNjp9TwDfAOQ/0?wx_fmt=jpeg',
-  followingCount: 685,
-  followerCount: 426,
-};
