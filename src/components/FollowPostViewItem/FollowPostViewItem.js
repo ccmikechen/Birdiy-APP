32b7aedaf3d4c69cec92e3f2859c1cd8f1ee2242
Mike Chen <ccmikechen@gmail.com>
2019-05-30 03:51:12 +0800

    Improve performance for FollowPostView

--- src/components/FollowPostViewItem/FollowPostViewItem.js
+++ src/components/FollowPostViewItem/FollowPostViewItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
   View,
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import { Surface } from 'react-native-paper';
+import { isEqual } from 'lodash';
 
 import ImageView from '../ImageView';
 import Avatar from '../Avatar';
@@ -14,58 +15,67 @@ import Size from '../../constants/Size';
 
 import styles from './styles';
 
-const FollowPostViewItem = ({ post, onPress, onUserPress }) => (
-  <Surface style={styles.container}>
-    <TouchableOpacity
-      style={styles.imageContainer}
-      onPress={() => onPress(post.author.id, post.id)}
-    >
-      <ImageView
-        style={styles.image}
-        uri={post.thumbnail ? post.thumbnail.image : null}
-        width={Size.followPostImageSize}
-      />
-    </TouchableOpacity>
-    <TouchableOpacity
-      style={styles.authorContainer}
-      onPress={() => onUserPress(post.author.id)}
-    >
-      <View style={styles.authorAvatarContainer}>
-        <Avatar
-          image={post.author.image}
-          onPress={() => onUserPress(post.author.id)}
-          size={Size.followPostAuthorImageSize}
-          borderRadius={Size.followPostAuthorImageSize / 2}
-        />
-      </View>
-      <View style={styles.authorNameContainer}>
-        <Text style={styles.authorName}>
-          {post.author.name}
-        </Text>
-      </View>
-    </TouchableOpacity>
-  </Surface>
-);
-
-FollowPostViewItem.propTypes = {
-  post: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    thumbnail: PropTypes.shape({
-      image: PropTypes.string,
-    }),
-    author: PropTypes.shape({
+export default class FollowPostViewItem extends Component {
+  static propTypes = {
+    post: PropTypes.shape({
       id: PropTypes.string.isRequired,
-      image: PropTypes.string,
-      name: PropTypes.string.isRequired,
-    }),
-  }).isRequired,
-  onPress: PropTypes.func,
-  onUserPress: PropTypes.func,
-};
+      thumbnail: PropTypes.shape({
+        image: PropTypes.string,
+      }),
+      author: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        image: PropTypes.string,
+        name: PropTypes.string.isRequired,
+      }),
+    }).isRequired,
+    onPress: PropTypes.func,
+    onUserPress: PropTypes.func,
+  };
 
-FollowPostViewItem.defaultProps = {
-  onPress: () => {},
-  onUserPress: () => {},
-};
+  static defaultProps = {
+    onPress: () => {},
+    onUserPress: () => {},
+  };
 
-export default FollowPostViewItem;
+  shouldComponentUpdate(nextProps) {
+    const { post } = this.props;
+    return !isEqual(post, nextProps.post);
+  }
+
+  render() {
+    const { post, onPress, onUserPress } = this.props;
+
+    return (
+      <Surface style={styles.container}>
+        <TouchableOpacity
+          style={styles.imageContainer}
+          onPress={() => onPress(post.author.id, post.id)}
+        >
+          <ImageView
+            style={styles.image}
+            uri={post.thumbnail ? post.thumbnail.image : null}
+            width={Size.followPostImageSize}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.authorContainer}
+          onPress={() => onUserPress(post.author.id)}
+        >
+          <View style={styles.authorAvatarContainer}>
+            <Avatar
+              image={post.author.image}
+              onPress={() => onUserPress(post.author.id)}
+              size={Size.followPostAuthorImageSize}
+              borderRadius={Size.followPostAuthorImageSize / 2}
+            />
+          </View>
+          <View style={styles.authorNameContainer}>
+            <Text style={styles.authorName}>
+              {post.author.name}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </Surface>
+    );
+  }
+}
